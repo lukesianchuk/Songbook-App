8b6053e83cf73cf62462e01d7881c3e3d36096f3
Luke <luke.sianchuk@gmail.com>
2022-08-23 17:18:34 -0400

    added external link functionality

--- songs/models.py
+++ songs/models.py
@@ -6,3 +6,4 @@ class Song(models.Model):
     artist = models.TextField()
     instrument = models.CharField(max_length=20)
     image = models.FilePathField(path="/img")
+    chordlink = models.URLField(default="https://www.ultimate-guitar.com/")
